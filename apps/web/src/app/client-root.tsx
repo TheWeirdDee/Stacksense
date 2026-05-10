81846e8b99762d948446d7a86b7c07bf4e4a6ca3
TheWeirdDee <divinenation1@gmail.com>
2026-05-11 00:12:51 +0100

    refactor: client-side entry point structure

--- apps/web/src/app/client-root.tsx
+++ apps/web/src/app/client-root.tsx
@@ -2,7 +2,6 @@
 import dynamic from 'next/dynamic'
 import { ReactNode } from 'react'
 
-// ssr: false must be inside a Client Component
 const ClientProviders = dynamic(() => import('@/lib/providers'), { ssr: false })
 
 export default function ClientRoot({ children }: { children: ReactNode }) {
